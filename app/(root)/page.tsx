7abedccfba91bef2493c8bc4c580f5db1b37ded5
MarcosViana-Brasil <marcos1968@gmail.com>
2024-08-19 17:23:38 -0300

    feat:  include logic PodcastCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@/components/ui/button'
+import PodcastCard from '@/components/PodcastCard'
+import { podcastData } from '@/constants'
 import React from 'react'
 
 const Home = () => {
@@ -6,6 +7,18 @@ const Home = () => {
         <div className='mt-9 flex flex-col gap-9'>
             <section className="flex flex-col gap-5">
                 <h1 className='text-20 font-bold text-white-1'>Trending Podcast</h1>
+
+                <div className="podcast_grid">
+                    {podcastData.map(({ id, title, description, imgURL }) => (
+                        <PodcastCard
+                            key={id}
+                            imgUrl={imgURL}
+                            title={title}
+                            description={description}
+                            postcastId={id}
+                        />
+                    ))}
+                </div>
             </section>
         </div>
     )
