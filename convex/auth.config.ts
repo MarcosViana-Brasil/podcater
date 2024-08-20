107f17b619ba307057795443d8d0d5e5b434fcf1
MarcosViana-Brasil <marcos1968@gmail.com>
2024-08-20 09:37:31 -0300

    feat: Make logic for sign-in and sign-up

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: 'https://welcomed-sloth-9.clerk.accounts.dev',
+            applicationID: 'convex',
+        },
+    ],
+}
+
+export default authConfig
